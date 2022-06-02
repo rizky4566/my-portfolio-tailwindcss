5724d377fe7c3fb58ab69a254dc48ad3262d9992
rizkykurniawan <rizkur4566@gmail.com>
2022-06-03 00:48:08 +0700

    menambahkan darkmode dll

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,4 +18,35 @@ const navMenu = document.querySelector('#nav-menu');
 hamburger.addEventListener('click', function() {             //ketika hamburger nya di klik kita akan jalan function berikut ini
     hamburger.classList.toggle('hamburger-active');            //toggle= jadi nambahin kelas baru atau ngermove class secara bersamaan (buka tutup)
     navMenu.classList.toggle('hidden');
-});     
+});  
+
+
+const checkbox = document.querySelector('#dark-toggle');
+      const html = document.querySelector('html');
+
+      checkbox.addEventListener('click', function(){
+        checkbox.checked ? html.classList.add('dark') : html.classList.remove('dark');
+      })
+
+
+
+
+// darkmode toggle yg episode baru
+// const darkToggle = document.querySelector('#dark-toggle');
+// const html = document.querySelector('html');
+
+// darkToggle.addEventListener('click', function() {
+//     if (darkToggle.checked) {
+//         html.classList.add('dark');
+//     } else {
+//         html.classList.remove('dark');
+//     }
+// }
+
+// click di luar hamburger
+// window.addEventListener('click', function (e) {
+//         if (e.target != hamburger && e.target != navMenu) {
+//             hamburger.classList.remove('hamburger-active');
+//             navMenu.classList.add('hidden');
+//         };
+//     });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
         primary: '#0ea5e9',
         secondary: '#64748b',
         dark: '#1e293b',
+        dark2: '#0f172a',
       },
     },
   },
